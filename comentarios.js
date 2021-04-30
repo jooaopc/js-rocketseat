d46133ac587298cc93d037a9f0ff2b746e051164
João Pedro <joao.ped.cs@gmail.com>
2021-04-30 17:56:55 -0300

    primeira commit

---
+++ comentarios.js
@@ -0,0 +1,11 @@
+// fazer comentarios e barra=barra
+
+/*
+
+isso é um bloco de comentarios ate fazer o contrario
+
+o codigo abaixo escreve no devtools, f12 no chrome
+*/
+
+console.log("Bem-vindos ao starter")
+console.log("outra coisa escrita")
